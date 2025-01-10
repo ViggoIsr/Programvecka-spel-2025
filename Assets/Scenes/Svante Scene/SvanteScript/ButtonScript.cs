a361c6e4e29ce3a1ebf4de1d3ddb860996be0473
ChingChongggg <svante.gustavssonblomberg@elev.ga.lbs.se>
2025-01-10 15:18:22 +0100

    TextMeshProshit, och egna scener

---
+++ Assets/Scenes/Svante Scene/SvanteScript/ButtonScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    [SerializeField] private string targetScene;
+
+    // Method called when the button is clicked
+    public void LoadTargetScene()
+    {
+        if (!string.IsNullOrEmpty(targetScene))
+        {
+            SceneManager.LoadScene(targetScene);
+        }
+        else
+        {
+            Debug.LogWarning("Target scene name is not set on " + gameObject.name);
+        }
+    }
+}
