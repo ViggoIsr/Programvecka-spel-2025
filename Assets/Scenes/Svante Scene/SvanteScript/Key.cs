e8cdeca957591644892314e5b579d468d18c8947
ChingChongggg <svante.gustavssonblomberg@elev.ga.lbs.se>
2025-01-17 11:08:56 +0100

    Dörrar

--- Assets/Scenes/Svante Scene/SvanteScript/Key.cs
+++ Assets/Scenes/Svante Scene/SvanteScript/Key.cs
@@ -6,6 +6,10 @@ public class Key : MonoBehaviour
     [SerializeField]
     private GameObject targetDoor;
 
+    // New sprite to be assigned to the door when the key is picked up
+    [SerializeField]
+    private Sprite newDoorSprite;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the object collided with is the player
@@ -21,6 +25,9 @@ public class Key : MonoBehaviour
                     doorCollider.enabled = false;
                     Debug.Log("Door unlocked!");
 
+                    // Change the door's sprite
+                    ChangeDoorSprite();
+
                     // Destroy the key after unlocking the door
                     Destroy(gameObject);
                 }
@@ -33,6 +40,27 @@ public class Key : MonoBehaviour
             {
                 Debug.LogWarning("No door assigned to this key!");
             }
+        }
+    }
+
+    private void ChangeDoorSprite()
+    {
+        if (newDoorSprite != null)
+        {
+            SpriteRenderer doorSpriteRenderer = targetDoor.GetComponent<SpriteRenderer>();
+            if (doorSpriteRenderer != null)
+            {
+                doorSpriteRenderer.sprite = newDoorSprite;
+                Debug.Log("Door sprite changed!");
+            }
+            else
+            {
+                Debug.LogWarning("The target door does not have a SpriteRenderer component!");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No new sprite assigned for the door!");
         }
     }
 }
